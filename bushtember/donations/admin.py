15091283f320b9277a1020d2288ddbeef009d9ac
Sean McIntyre <boxysean@gmail.com>
2015-08-26 01:10:28 -0400

    updated admin

--- bushtember/donations/admin.py
+++ bushtember/donations/admin.py
@@ -5,14 +5,14 @@ from django.conf import settings
 from .models import Donation
 
 class DonationAdmin(admin.ModelAdmin):
-	exclude = ('image_path', 'charge')
+	exclude = ('uploaded_image', 'charge')
 	list_display = ('token', 'customer', 'date_created')
-	readonly_fields = ('image_path_readonly', 'amount_charged')
+	readonly_fields = ('uploaded_image_readonly', 'amount_charged')
 
-	def image_path_readonly(self, instance):
-		return mark_safe('<img width=400 src="{0}{1}"><br><a href="{0}{1}">Download</a>'.format(settings.MEDIA_URL, instance.image_path))
+	def uploaded_image_readonly(self, instance):
+		return mark_safe('<img width=400 src="{0}{1}"><br><a href="{0}{1}">Download</a>'.format(settings.MEDIA_URL, instance.uploaded_image))
 
-	image_path_readonly.short_description = "Image uploaded by user"
+	uploaded_image_readonly.short_description = "Image uploaded by user"
 
 	def amount_charged(self, instance):
 		return '${0}'.format(instance.charge.amount)

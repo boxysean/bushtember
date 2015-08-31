d3d2fa0f06a93097e20d2bdc85b829b7f09b4e51
Sean McIntyre <boxysean@gmail.com>
2015-08-30 22:43:28 -0400

    update?

--- bushtember/bushtember/settings.py
+++ bushtember/bushtember/settings.py
@@ -134,11 +134,11 @@ if DEBUG:
     MEDIA_URL = '/media/'
 else:
     STATICFILES_LOCATION = 'static'
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'bushtember.custom_storages.StaticStorage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
     MEDIAFILES_LOCATION = 'media'
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'bushtember.custom_storages.MediaStorage'
 
 # Stripe settings
 

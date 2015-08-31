3dfb9f53a081f4bb3f85cc75bfbcfa72e57d98a9
Sean McIntyre <boxysean@gmail.com>
2015-08-30 22:17:57 -0400

    updated var names

--- bushtember/bushtember/settings.py
+++ bushtember/bushtember/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!$ks1p+4l9$#w7#^iin0tlb!@giex0dg)v+0!_$z@n$qh%j2r7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!$ks1p+4l9$#w7#^iin0tlb!@giex0dg)v+0!_$z@n$qh%j2r7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 

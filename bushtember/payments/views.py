3ce19a63d6a7b2be9d60335b6f5d1c4e50fdd06d
Sean McIntyre <boxysean@gmail.com>
2015-08-30 23:30:11 -0400

    +debugging

--- bushtember/payments/views.py
+++ bushtember/payments/views.py
@@ -193,6 +193,11 @@ def donate(request, form_class=DonateForm):
             charge.send_receipt()
 
             response = redirect(reverse('donations.views.upload_photo_view', kwargs={'donation_token': donation.token}))
+
+            print 'RESPONSE'
+            for key in dir(response):
+                print key, getattr(response, key)
+
             return response
 
         except stripe.StripeError as e:
